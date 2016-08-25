09aa3438806571851349484dbd486cef27f1cf13
zhangjukai <jk_zhangjukai@163.com>
2016-08-25 17:29:57 +0800

    初始化项目

---
+++ src/main/java/com/fcore/boot/Application.java
@@ -0,0 +1,20 @@
+package com.fcore.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+	
+	@RequestMapping("/")
+    public String start() {
+		return "Hello World!";
+    }
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
---
+++ src/main/java/com/fcore/boot/controller/IndexController.java
@@ -0,0 +1,21 @@
+package com.fcore.boot.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	private static Logger log = LoggerFactory.getLogger(IndexController.class);
+	
+	@RequestMapping(value = "/index")
+	public String index(Model model) {
+		model.addAttribute("msg", "测试消息");
+		log.info("测试日志记录");
+		return "index";
+	}
+	
+}
